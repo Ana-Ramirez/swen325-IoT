c176e4960a15b749fb74db477541061becb9b4b2
anaramirez <ramireana@myvuw.ac.nz>
2018-10-24 00:10:38 +1300

    accessing server data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,82 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var Paho : any;
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  private mqttStatus: string = 'Disconnected';
+  private mqttClient: any = null;
+  private message: any = '';
+  private messageToSend: string = 'Your message';
+  private topic: string = 'swen325/a3';
+  private clientId: string = 'ramireana'
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  public connect = () => {
+    this.mqttStatus = 'Connecting...';
+    this.mqttClient = new Paho.MQTT.Client('m15.cloudmqtt.com', 30928, '/mqtt', this.clientId);
+    //this.mqttClient = new Paho.MQTT.Client('barretts.ecs.vuw.ac.nz', 8883, '/mqtt', this.clientId);
+
+    // set callback handlers
+    this.mqttClient.onConnectionLost = this.onConnectionLost;
+    this.mqttClient.onMessageArrived = this.onMessageArrived;
+
+    // connect the client
+    console.log('Connecting to mqtt via websocket');
+    this.mqttClient.connect({timeout:10, userName:'zlkeyfjr', password:'__70vczj02nW', useSSL:true, onSuccess:this.onConnect, onFailure:this.onFailure});
+    //this.mqttClient.connect({timeout:10, useSSL:false, onSuccess:this.onConnect, onFailure:this.onFailure});
+  }
+
+  public disconnect () {
+    if(this.mqttStatus == 'Connected') {
+      this.mqttStatus = 'Disconnecting...';
+      this.mqttClient.disconnect();
+      this.mqttStatus = 'Disconnected';
+    }
+  }
+
+  public sendMessage () {
+    if(this.mqttStatus == 'Connected') {
+      this.mqttClient.publish(this.topic, this.messageToSend);
+    }
+  }
+
+  public onConnect = () => {
+    console.log('Connected');
+    this.mqttStatus = 'Connected';
+
+    console.log("HERE");
+    //console.log(this.mqttClient.getData());
+
+    // subscribe
+    this.mqttClient.subscribe(this.topic);
+  }
+
+  public onFailure = (responseObject) => {
+    console.log('Failed to connect');
+    this.mqttStatus = 'Failed to connect';
+  }
+
+  public onConnectionLost = (responseObject) => {
+    if (responseObject.errorCode !== 0) {
+      this.mqttStatus = 'Disconnected';
+    }
+  }
+
+  public onMessageArrived = (message) => {
+
+    this.message = message.payloadString;
+    console.log('Received message:' + this.message);
+  }
 }
+
